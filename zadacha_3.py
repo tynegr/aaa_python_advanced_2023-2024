0645f5aaa45f5d1703d0cc0383fc3afe3c19b3d6
tynegr <122130536+tynegr@users.noreply.github.com>
2023-12-14 17:51:50 +0300

    Update zadacha_3.py

--- zadacha_3.py
+++ zadacha_3.py
@@ -1,8 +1,29 @@
 import sys
+from typing import Callable
 
 
-def redirect_output(filepath):
-    def decorator(func):
+def redirect_output(filepath: str) -> Callable:
+    """
+    Decorator that redirects the standard output to a file.
+
+    Parameters:
+        filepath (str): The path to the file where the output will
+        be redirected.
+
+    Returns:
+        Callable: A decorator function.
+    """
+    def decorator(func: Callable) -> Callable:
+        """
+        Decorator function that wraps the original function
+        and redirects its output.
+
+        Parameters:
+            func (Callable): The function to be decorated.
+
+        Returns:
+            Callable: The wrapper function.
+        """
         def wrapper(*args, **kwargs):
             original_stdout = sys.stdout
             try:
@@ -17,14 +38,19 @@ def redirect_output(filepath):
 
 
 @redirect_output('./function_output.txt')
-def calculate():
+def calculate_powers():
+    """
+    Calculates powers of numbers and prints the result to the
+    redirected output.
+    """
     for power in range(1, 5):
         for num in range(1, 20):
             print(num ** power, end=' ')
         print()
 
 
-calculate()
+calculate_powers()
+
 with open('./function_output.txt', 'r') as f:
-    content = f.read()
-    print(content)
+    data = f.read()
+    print(data)
